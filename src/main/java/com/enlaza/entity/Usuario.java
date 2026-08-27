2b3e133b6d0e731007955d06f4d852298437f756
ANDRIU VARGAS CARDENAS <vargasandriu44@gmail.com>
2026-08-27 11:25:23 -0500

    Implementacion entidad Usuario

--- src/main/java/com/enlaza/entity/Usuario.java
+++ src/main/java/com/enlaza/entity/Usuario.java
@@ -1,5 +1,31 @@
 package com.enlaza.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Usuario {
-    // TODO: Implementar entidad Usuario
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @Column(unique = true)
+    private String correo;
+
+    private Integer edad;
+
+    private String genero;
+
+    private String ciudad;
+
+    @Column(length = 500)
+    private String biografia;
 }
