dc6341faaafbd71583388df564771e2630c3ccdc
ANDRIU VARGAS CARDENAS <vargasandriu44@gmail.com>
2026-08-27 11:20:26 -0500

    Implementacion Suscripcion y SuscripcionRepository

--- src/main/java/com/enlaza/repository/SuscripcionRepository.java
+++ src/main/java/com/enlaza/repository/SuscripcionRepository.java
@@ -1,5 +1,14 @@
 package com.enlaza.repository;
 
-public interface SuscripcionRepository {
-    // TODO: Implementar repositorio Suscripcion
+import com.enlaza.entity.Suscripcion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SuscripcionRepository extends JpaRepository<Suscripcion, Long> {
+
+    List<Suscripcion> findByUsuarioId(Long usuarioId);
+
+    List<Suscripcion> findByActivaTrue();
+
 }
