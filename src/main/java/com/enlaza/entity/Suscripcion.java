b86728d3a733f8984d52ae21e967202d9f1defb2
ANDRIU VARGAS CARDENAS <vargasandriu44@gmail.com>
2026-08-27 11:18:03 -0500

    Implementacion entidad Suscripcion

--- src/main/java/com/enlaza/entity/Suscripcion.java
+++ src/main/java/com/enlaza/entity/Suscripcion.java
@@ -1,5 +1,38 @@
 package com.enlaza.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "suscripciones")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Suscripcion {
-    // TODO: Implementar entidad Suscripcion
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long usuarioId;
+
+    @Enumerated(EnumType.STRING)
+    private TipoSuscripcion tipo;
+
+    @Builder.Default
+    private LocalDateTime fechaInicio = LocalDateTime.now();
+
+    private LocalDateTime fechaFin;
+
+    @Builder.Default
+    private Boolean activa = true;
+
+    public enum TipoSuscripcion {
+        ESTANDAR,
+        PREMIUM,
+        PLATINO
+    }
 }
