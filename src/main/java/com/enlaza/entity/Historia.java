4bb99ae0764f684852a4a8f67814805f9daa507b
ANDRIU VARGAS CARDENAS <vargasandriu44@gmail.com>
2026-08-27 11:16:49 -0500

    Implementacion entidad Historia

--- src/main/java/com/enlaza/entity/Historia.java
+++ src/main/java/com/enlaza/entity/Historia.java
@@ -1,5 +1,32 @@
 package com.enlaza.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "historias")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Historia {
-    // TODO: Implementar entidad Historia
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long usuarioId;
+
+    @Column(length = 500)
+    private String contenido;
+
+    @Builder.Default
+    private LocalDateTime fechaCreacion = LocalDateTime.now();
+
+    private LocalDateTime fechaExpiracion;
+
+    @Builder.Default
+    private Boolean activa = true;
 }
