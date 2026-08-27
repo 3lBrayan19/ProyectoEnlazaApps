06ae0b285b019bb644ac39b6067a506fdcc4df2e
ANDRIU VARGAS CARDENAS <vargasandriu44@gmail.com>
2026-08-27 11:26:26 -0500

    Implementacion entidad Foto

--- src/main/java/com/enlaza/entity/Foto.java
+++ src/main/java/com/enlaza/entity/Foto.java
@@ -1,5 +1,21 @@
 package com.enlaza.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "fotos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Foto {
-    // TODO: Implementar entidad Foto
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String urlFoto;
+
+    private Long usuarioId;
 }
