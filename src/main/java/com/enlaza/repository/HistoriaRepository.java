f62cf30ad7c2aa1e59f581aea8641c149f0eb6f3
ANDRIU VARGAS CARDENAS <vargasandriu44@gmail.com>
2026-08-27 11:18:59 -0500

    Implementacion HistoriaRepository

--- src/main/java/com/enlaza/repository/HistoriaRepository.java
+++ src/main/java/com/enlaza/repository/HistoriaRepository.java
@@ -1,5 +1,14 @@
 package com.enlaza.repository;
 
-public interface HistoriaRepository {
-    // TODO: Implementar repositorio Historia
+import com.enlaza.entity.Historia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface HistoriaRepository extends JpaRepository<Historia, Long> {
+
+    List<Historia> findByUsuarioId(Long usuarioId);
+
+    List<Historia> findByActivaTrue();
+
 }
